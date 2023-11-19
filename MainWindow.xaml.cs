2c9548e26864786348b61ce5d035c4574d2a7e08
frei480 <frei741@gmail.com>
2023-11-19 13:24:51 +0500

    get current version from site

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using HtmlAgilityPack;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
 using WinForm = System.Windows.Forms;
 
 
@@ -13,6 +17,7 @@ namespace WebDownloader
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
         private void btnFolder_Click(object sender, RoutedEventArgs e)
@@ -31,6 +36,34 @@ namespace WebDownloader
         {
 
             Downloader.Start(folder, this);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Версия на сайте:
+            GetVersionfromWeb();
+        }
+
+        private async Task GetVersionfromWeb()
+        {
+            // Xpatch //*[@id="page_maincontainer"]/div[2]/div[1]/div[1]/div[1]/text()
+            // Full Xpatch /html/body/div[7]/div[2]/div[1]/div[1]/div[1]/text()
+            string url = "https://www.tflex.ru/downloads/";
+            string getVersion = await Task.Run(() => { 
+            var web = new HtmlAgilityPack.HtmlWeb();
+            HtmlDocument doc =  web.Load(url);
+            return doc.DocumentNode.SelectNodes("//*[@id=\"page_maincontainer\"]/div[2]/div[1]/div[1]/div[1]")[0].InnerText;            
+            });
+
+            string pattern = @"[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+";
+            string version = "";
+            RegexOptions options = RegexOptions.Multiline;
+            foreach (Match m in Regex.Matches(getVersion, pattern, options))
+            {
+                version = m.Value;
+            }
+
+            labelVersion.Content = "Версия на сайте: " + version;
         }
     }
 }
